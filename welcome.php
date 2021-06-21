a97076eed438abfb79ae6cc11d7169e7b28b8e1b
adrika saha <adrika.saha.55@gmail.com>
2021-06-22 00:05:01 +0600

    code add

---
+++ welcome.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style> error{color=red;}</style>
+    <title>welcome</title>
+</head>
+<body>
+<?php
+$fname=$lname=$gender=$DOB=$Religion=$username=$email=$password="";
+$fnameErr = $lnamErr=$DOBErr=$emailErr=$genderErr=$ReligionErr=$usernameErr=$passwordErr="";
+echo "<h2>welcome:</h2>";
+echo "<b>First Name: </b>".$fname;
+echo "<br>";
+echo "<b>LAST Name: </b>".$lname;
+echo "<br>";
+echo "<b>Email: </b>".$email;
+echo "<br>";
+echo "<b>DOB: </b>".$DOB;
+echo "<br>";
+echo "<b>Religion: </b>".$Religion;
+echo "<br>";
+echo "<b>Gender: </b>".$gender;
+echo "<br>";
+echo "<b>User name: </b>".$gender;
+echo "<br>";
+?>
+</body>
+</html>
